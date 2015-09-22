d7d50f899da8d1c4bded705008a12a44b8ed6136
Adam Szajuk <adam.szajuk@thecodeine.com>
2015-09-22 10:49:02 +0200

    TUNACMS-21 AdminBundle

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 php Behat/Fixtures/app/console --env=test --no-debug doctrine:schema:drop --force
 php Behat/Fixtures/app/console --env=test --no-debug doctrine:schema:create
+php Behat/Fixtures/app/console --env=test --no-debug doctrine:schema:update --force
 php Behat/Fixtures/app/console --env=test --no-debug doctrine:fixtures:load
 php Behat/Fixtures/app/console --env=test --no-debug assets:install Behat/Fixtures/web/
 php Behat/Fixtures/app/console assetic:dump
