7cabf575c42f6d1848dc49560c60b8a286cdaf03
Adam Szajuk <adam.szajuk@thecodeine.com>
2015-10-01 14:52:08 +0200

    TUNACMS-21 AdminBundle / run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,20 @@ nohup bash -c "php Behat/Fixtures/app/console --env=test --no-debug doctrine:sch
 nohup bash -c "php Behat/Fixtures/app/console --env=test --no-debug doctrine:fixtures:load -n 2>&1 &" && sleep 1;
 nohup bash -c "php Behat/Fixtures/app/console --env=test --no-debug cache:clear 2>&1 &" && sleep 1;
 nohup bash -c "php Behat/Fixtures/app/console --env=test --no-debug server:run 2>&1 &" && sleep 1;
-vendor/bin/behat -f progress -n;
-vendor/bin/phpspec run --format=pretty --no-code-generation;
-kill $(lsof -i :8000 | grep php | awk '{print $2}')
+
+exit_code=0;
+commands=(
+    "vendor/bin/behat -f progress -n"
+    "vendor/bin/phpspec run --format=pretty --no-code-generation"
+    "kill $(lsof -i :8000 | grep php | awk '{print $2}')"
+)
+
+for i in "${commands[@]}"
+do
+   $i
+   if [ $? -ne 0 ]; then
+      exit_code=1
+   fi
+done
+
+exit $exit_code
