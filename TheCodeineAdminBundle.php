01824d684cdff19c1c4ac12939fcca2001615b3a
Adam Szajuk <adam.szajuk@thecodeine.com>
2015-10-02 11:09:04 +0200

    TUNACMS-21 AdminBundle

--- TheCodeineAdminBundle.php
+++ TheCodeineAdminBundle.php
@@ -5,9 +5,4 @@ namespace TheCodeine\AdminBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TheCodeineAdminBundle extends Bundle
-{
-    public function getParent()
-    {
-        return 'FOSUserBundle';
-    }
-}
+{}
