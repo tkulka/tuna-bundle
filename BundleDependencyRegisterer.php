a10b629b73523e245ee2b2439ecd834721b33291
Marek Fijas <fijas@juice.pl>
2016-06-13 15:02:20 +0200

    one-package BundleDependencyRegisterer fix + UserBundle

--- BundleDependencyRegisterer.php
+++ BundleDependencyRegisterer.php
@@ -7,15 +7,15 @@ class BundleDependencyRegisterer
     static public function register(array &$bundles)
     {
         $dependencies = array(
-            new TheCodeine\AdminBundle\TheCodeineAdminBundle(),
-            new TheCodeine\EditorBundle\TheCodeineEditorBundle(),
-            new TheCodeine\PageBundle\TheCodeinePageBundle(),
-            new TheCodeine\ImageBundle\TheCodeineImageBundle(),
-            new TheCodeine\VideoBundle\TheCodeineVideoBundle(),
-            new TheCodeine\GalleryBundle\TheCodeineGalleryBundle(),
-            new TheCodeine\TagBundle\TheCodeineTagBundle(),
-            new TheCodeine\NewsBundle\TheCodeineNewsBundle(),
-            new TheCodeine\UserBundle\TheCodeineUserBundle(),
+            new \TheCodeine\AdminBundle\TheCodeineAdminBundle(),
+            new \TheCodeine\EditorBundle\TheCodeineEditorBundle(),
+            new \TheCodeine\PageBundle\TheCodeinePageBundle(),
+            new \TheCodeine\ImageBundle\TheCodeineImageBundle(),
+            new \TheCodeine\VideoBundle\TheCodeineVideoBundle(),
+            new \TheCodeine\GalleryBundle\TheCodeineGalleryBundle(),
+            new \TheCodeine\TagBundle\TheCodeineTagBundle(),
+            new \TheCodeine\NewsBundle\TheCodeineNewsBundle(),
+            new \TheCodeine\UserBundle\TheCodeineUserBundle(),
         );
 
         foreach ($bundles as $bundle) {
