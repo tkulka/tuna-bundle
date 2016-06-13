c7f7c1885bfbd80802503a889681acffb7b646e7
Artur Stula <a.stula@juice.pl>
2016-06-13 17:19:08 +0200

    OE-54 / Nadpisywanie newsControllera

---
+++ Controller/NewsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TheCodeine\AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+
+class NewsController extends \TheCodeine\NewsBundle\Controller\NewsController
+{
+}
