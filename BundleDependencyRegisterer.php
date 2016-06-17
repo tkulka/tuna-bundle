4b47109f715af2920d53818dab41c6e384f9e444
konjag <konrad.jaguszewski@gmail.com>
2016-06-17 12:57:06 +0200

    TUNACMS-59 / TranslationBundle

--- BundleDependencyRegisterer.php
+++ BundleDependencyRegisterer.php
@@ -16,6 +16,7 @@ class BundleDependencyRegisterer
             new \TheCodeine\TagBundle\TheCodeineTagBundle(),
             new \TheCodeine\NewsBundle\TheCodeineNewsBundle(),
             new \TheCodeine\UserBundle\TheCodeineUserBundle(),
+            new \TheCodeine\TranslationBundle\TheCodeineTranslationBundle(),
         );
 
         foreach ($bundles as $bundle) {
