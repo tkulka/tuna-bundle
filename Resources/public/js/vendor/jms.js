438fd6746ee2de406d697c79bdf018b06b66828d
konjag <konrad.jaguszewski@gmail.com>
2016-07-07 12:07:08 +0200

    OE-79 / czyszczenie cache

--- Resources/public/js/vendor/jms.js
+++ Resources/public/js/vendor/jms.js
@@ -2,6 +2,23 @@ var jms = JMSTranslationManager;
 JMSTranslationManager = function() {
     jms.call(this, updateMessagePath, isWritable, infoMessages);
 
+    $('.refresh-translations').on('click', function(e) {
+        var $this = $(this);
+        e.preventDefault();
+        $.ajax({
+            url: 'refresh',
+            beforeSend: function() {
+                $this.closest('.admin-option-container').find('.refresh-message').remove();
+            },
+            error: function() {
+                $this.before('<div class="refresh-message error">Nie udało się odświeżyć tłumaczeń.</div>');
+            },
+            success: function() {
+                $this.before('<div class="refresh-message">Odświeżono tłumaczenia na stronie.</div>');
+            }
+        })
+    });
+
     this.translation.ajax = {
         type: 'POST',
         headers: {'X-HTTP-METHOD-OVERRIDE': 'PUT'},
--- src/Tuna/Bundle/TranslationBundle/Controller/CacheController.php
+++ src/Tuna/Bundle/TranslationBundle/Controller/CacheController.php
@@ -2,18 +2,25 @@
 
 namespace TheCodeine\TranslationBundle\Controller;
 
-use JMS\TranslationBundle\Controller\ApiController;
-use Symfony\Component\HttpFoundation\Request;
+use JMS\DiExtraBundle\Annotation as DI;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Filesystem\Filesystem;
 
-class CacheController extends ApiController
+class CacheController extends Controller
 {
     /**
-     * @Route("/translations/save")
+     * @Route("/refresh")
+     *
+     * @return Response
      */
     public function clearCache()
     {
         $fs = new Filesystem();
         $fs->remove($this->container->getParameter('kernel.cache_dir'));
+
+        return new Response();
     }
 }
