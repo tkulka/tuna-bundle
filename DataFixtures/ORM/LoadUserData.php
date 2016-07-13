9ae722562b75e876be19796b27e321490031c7c2
Marek Fijas <fijas@juice.pl>
2016-07-13 12:12:05 +0200

    CODEINE-27 don't add another admin user in fixture

--- DataFixtures/ORM/LoadUserData.php
+++ DataFixtures/ORM/LoadUserData.php
@@ -5,6 +5,7 @@ namespace TheCodeine\AdminBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use FOS\UserBundle\Entity\UserManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -28,15 +29,21 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
      */
     public function load(ObjectManager $om)
     {
+        /* @var UserManager $userManager */
         $userManager = $this->container->get('fos_user.user_manager');
-        $user = $userManager->createUser();
-        $user->setEnabled(true);
-        $user->setPlainPassword('admin');
-        $user->setUsername('admin');
-        $user->setEmail('fake@thecodeine.com');
-        $user->addRole('ROLE_ADMIN');
+        $email = 'fake@thecodeine.com';
+        $username = 'admin';
 
-        $userManager->updateUser($user);
+        if ($userManager->findUserByUsername($username) == null && $userManager->findUserByEmail($email) == null) {
+            $user = $userManager->createUser();
+            $user->setEnabled(true);
+            $user->setPlainPassword('admin');
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->addRole('ROLE_ADMIN');
+
+            $userManager->updateUser($user);
+        }
     }
 
 
