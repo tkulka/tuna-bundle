1fc7218dd8076002c41e62e04df38876f09940fb
Artur Stula <a.stula@juice.pl>
2016-07-20 17:09:53 +0200

    TUNACMS-79 / Zwiekszony dropzone dla cover image

--- Resources/public/js/views/MainImageView.js
+++ Resources/public/js/views/MainImageView.js
@@ -3,46 +3,6 @@
         events: {
             'click [data-action="remove"]': 'onRemove'
         },
-        initialize: function () {
-            this.initDropzone();
-        },
-        initDropzone: function () {
-            var oThis = this;
-
-           this.$('.main-image').dropzone({
-                url: '/admin/news/image/upload',
-                maxFilesize: 2,
-                acceptedFiles: '.jpg, .jpeg',
-                paramName: 'the_codeine_image_request[file]',
-                clickable: '.main-image .btn',
-                maxFiles: 1,
-                addedfile: function () {},
-                error: function (file, errorMessage) {
-                    oThis.onSending();
-                    alert(errorMessage);
-                },
-                init: function () {
-                    this.on("success", function(file, response) {
-                        oThis.showImagePreview(response);
-                    });
-
-                    this.on("queuecomplete", function () {
-                        oThis.onSending();
-                        this.removeAllFiles();
-                    });
-
-                    this.on("sending", function() {
-                        oThis.onSending();
-                    });
-                }
-            });
-        },
-        showImagePreview: function (response) {
-            this.$('.main-image .image').html('<div><img src="'+response.path+'"><span class="remove">Usuń</span></div>');
-        },
-        onSending: function () {
-            this.$('.main-image .btn').toggleClass('loading');
-        },
         onRemove: function (event) {
             this.$('.image').empty();
         }
--- Resources/public/js/views/OptionsView.js
+++ Resources/public/js/views/OptionsView.js
@@ -7,12 +7,53 @@
 
         initialize: function () {
             this.$el.addClass('magictime');
+            this.initDropzone();
 
             new tuna.view.MainImageView({
                 el: this.$('.thecodeine_admin_main_image')
             });
         },
+        initDropzone: function () {
+            var oThis = this;
 
+            this.$el.dropzone({
+                url: '/admin/news/image/upload',
+                maxFilesize: 2,
+                acceptedFiles: '.jpg, .jpeg',
+                paramName: 'the_codeine_image_request[file]',
+                clickable: '.thecodeine_admin_main_image .btn',
+                maxFiles: 1,
+                addedfile: function () {},
+                error: function (file, errorMessage) {
+                    oThis.onSending();
+                    alert(errorMessage);
+                },
+                init: function () {
+                    this.on("success", function(file, response) {
+                        oThis.showImagePreview(response);
+                    });
+
+                    this.on("queuecomplete", function () {
+                        oThis.onSending();
+                        this.removeAllFiles();
+                    });
+
+                    this.on("sending", function() {
+                        oThis.onSending();
+                    });
+                }
+            }).on('dragbetterenter', function() {
+                oThis.$el.addClass('drag-over');
+            }).on('dragbetterleave', function() {
+                oThis.$el.removeClass('drag-over');
+            });
+        },
+        showImagePreview: function (response) {
+            this.$('.thecodeine_admin_main_image .image').html('<div><img src="'+response.path+'"><span class="remove">Usuń</span></div>');
+        },
+        onSending: function () {
+            this.$('.main-image .btn').toggleClass('loading');
+        },
         _onGalleryOpen: function () {
             $('.admin-gallery-container').trigger('open');
         },
