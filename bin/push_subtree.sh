3d9ad2fe48da3bf38620fd5af4dcdc7b85c20957
Marek Fijas <mrfijas@users.noreply.github.com>
2017-02-13 10:39:25 +0100

    added rejoin flag to git subtree split command (#17)

--- bin/push_subtree.sh
+++ bin/push_subtree.sh
@@ -8,5 +8,5 @@ currentBranch=${2:-`git rev-parse --abbrev-ref HEAD`}
 
 bash bin/add_bundle_remote.sh $1
 
-git subtree split --prefix=src/Tuna/Bundle/$bundleName -b $splitBranch
+git subtree split --rejoin --prefix=src/Tuna/Bundle/$bundleName -b $splitBranch
 git push --force $remoteName $splitBranch:$currentBranch
