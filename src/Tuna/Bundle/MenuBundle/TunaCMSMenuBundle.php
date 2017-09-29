f646e2962a8e6a04bbec32748ea81adc5135a4de
Tomasz Kulka <tomasz.kulka@thecodeine.com>
2017-09-29 16:02:31 +0200

    fix MenuTypesPass

--- src/Tuna/Bundle/MenuBundle/TunaCMSMenuBundle.php
+++ src/Tuna/Bundle/MenuBundle/TunaCMSMenuBundle.php
@@ -4,12 +4,12 @@ namespace TunaCMS\Bundle\MenuBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use TunaCMS\Bundle\MenuBundle\DependencyInjection\Compiler\MenuTypesPassTest;
+use TunaCMS\Bundle\MenuBundle\DependencyInjection\Compiler\MenuTypesPass;
 
 class TunaCMSMenuBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(new MenuTypesPassTest());
+        $container->addCompilerPass(new MenuTypesPass());
     }
 }
